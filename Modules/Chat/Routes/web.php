b34b00e7c7d42827e28a605338ddfeaef219ee4d
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-12-29 18:40:14 +0700

    source code landing page woody

--- Modules/Chat/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Chat\Http\Controllers\{UserController, DashboardController};
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('/dashboard', DashboardController::class);
-    Route::resource('/user', UserController::class);
-});
